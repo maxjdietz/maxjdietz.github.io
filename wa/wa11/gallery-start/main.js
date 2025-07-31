bcaf8b433f7ba015a337ada30ae794342f3f8d6a
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2025-07-31 00:15:58 -0600

    Update main.js

--- wa/wa11/gallery-start/main.js
+++ wa/wa11/gallery-start/main.js
@@ -6,12 +6,13 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const imgArr = ["images/pic1.jpg","images/pic2.jpg","images/pic3.jpg","images/pic4.jpg", "images/pic5.jpg"];
+let brightChecker = 0;
 
 /* Declaring the alternative text for each image file */
 function displayImg(img){
     console.log(img);
    displayedImage.src = img.src;
-    displayedImage.setAttribute('alt', img);
+    displayedImage.alt = img.alt;
     return;
 }
 
@@ -30,4 +31,24 @@ for (i = 0; i < 5; i++){
     console.log("1");
 }
 
+function lightDark(lightChecker){
+    console.log(lightChecker);
+    if (lightChecker === 0){
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+        brightChecker++;
+    }
+    else{
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+        brightChecker--;
+
+    }
+}
+
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click",  (event) => {
+        lightDark(brightChecker);
+    });
