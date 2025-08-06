dd7502e858ed70dbb974e8d1c8928fd8527cb7b7
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2025-08-06 00:51:07 -0600

    finalPUSH1

--- Final/final.js
+++ Final/final.js
@@ -1,9 +1,11 @@
 const powerButton = document.querySelector("button");
 const powerMeter = document.querySelector(".meter");
+const powerLevel = document.querySelector(".number");
 
-powerButton.addEventListener("click", powerBar);
-let loopBar = 1;
-let clickChecker = 0;
+powerButton.addEventListener("mousedown", powerBar);
+
+let holdChecker = false;
+
 
 
 
@@ -12,17 +14,56 @@ let clickChecker = 0;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+function powerLvlFunc(power){
+  if(power < 10){
+     powerLevel.textContent = 0;
+
+  }
+  else{
+    let powerString = power.toString()
+     powerLevel.textContent = powerString[0];
+
+  }
+  
+   
+}
 
 async function powerBar(){
     console.log("test");
+    holdChecker = true;
+    let loopBar = 1;
     let increase = 0;
-    while(loopBar === 1){
+    while (holdChecker === true){
+      powerButton.addEventListener("mouseup", () => {
+        holdChecker = false;
+        loopBar = 2;
+        powerLvlFunc(increase);
+        return;
        
+
+      });
+
+      while(loopBar === 1){
+      
         console.log(increase);
-        await sleep(10);
+        await sleep(3);
         powerMeter.style.height = increase + "%";
+        if (increase == 100){
+          loopBar--;
+        }
         increase++;
 
+      }
+        while(loopBar === 0){
+        
+        console.log(increase);
+        await sleep(3);
+        powerMeter.style.height = increase + "%";
+        if (increase == 0){
+          loopBar++;
+        }
+        increase--;
+      }
     }
     
 
