cbc9417724e51ca7e0a29b1ed4294cf34a50917f
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2025-08-07 12:58:44 -0600

    FINALPt4

--- Final/final.js
+++ Final/final.js
@@ -5,18 +5,119 @@ const testButton = document.querySelector(".test");
 const line = document.querySelector(".line");
 const canvas = document.querySelector('.ballContainer');
 const ctx = canvas.getContext('2d');
+const phoneNumContainer = document.querySelector(".phoneNum")
 
-let speed = 0;
+const speed = 5;
 const speedOfMeter = 3;
+let loopBar = 1;
+let holdDown = true;
+let angle = 90;
+let undraw = 0;
+let phoneNum = 0;
 
-let ball = {
-    x: canvas.width/2,
-    y: canvas.height -30,
-    radius: 5,
-    color: "green",
-    dx: 0,
-    dy: 0
-  };
+function getRandomInteger() {
+  min = Math.ceil(100); // Ensure min is an integer
+  max = Math.floor(400); // Ensure max is an integer
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// let ball = {
+//     x: canvas.width/2,
+//     y: canvas.height -30,
+//     radius: 5,
+//     color: "green",
+//     dx: 0,
+//     dy: 0
+//   };
+
+
+class Goals{
+  constructor(x, y , radius, color, number){
+   this.x = x;
+   this.y = y;
+   this.radius = radius;
+   this.color = color;
+   this.number = number;
+  }
+  draw(){
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+  ctx.font = "30px fantasy";
+  ctx.textAlign = "center";
+  
+  ctx.fill();
+     ctx.strokeStyle = "black";
+    ctx.strokeText(phoneNum, goal.x , goal.y);
+  }
+
+}
+class Ball{
+  constructor(x, y, velX, velY, color, size){
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+    draw(){
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fill();
+    }
+    update(){
+      if(this.x + this.size >= canvas.width){
+        this.velX = -this.velX;
+        }
+      if(this.x - this.size <= 0){
+      this.velX = -this.velX;
+      }
+      if(this.y + this.size >= canvas.height){
+      this.velY = -this.velY;
+      }
+      if(this.y - this.size <= 0){
+      this.velY = -this.velY;
+      }
+      this.x += this.velX;
+      this.y += this.velY;
+      }
+}
+let ball = new Ball(250, 450, 0, 0, "red", 20);
+
+
+
+function loop(){
+  if (undraw === 1){
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    ball.velX = 0;
+    ball.velY =0;
+
+    ball.x = 250;
+    ball.y = 450;
+
+  }
+  else if((getDistance(ball.x, ball.y, goal.x, goal.y) < goal.radius)){
+    ball.velX = 0;
+    ball.velY =0;  
+    ball.x = 250;
+    ball.y = 450;
+    phoneNumContainer.textContent = phoneNumContainer.textContent + phoneNum;
+
+  }
+  
+ ctx.fillStyle = "rgb(255 255 255)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ball.draw();
+  goal.draw();
+  ball.update();
+ 
+  requestAnimationFrame(loop);
+
+  
+}
+
 
 
 //STEP1---------------------------------------------------------
@@ -24,8 +125,10 @@ powerButton.addEventListener("mousedown", powerBar); //hold down power button fo
 
 
 async function powerBar(){
-    holdDown = true;
-    let loopBar = 1; //for moving up and back down
+  holdDown = true;
+  loopBar = 1;
+   
+    //for moving up and back down
     let increase = 0; //for height of red meter
      //ms for sleep func
     while (holdDown === true){
@@ -34,19 +137,27 @@ async function powerBar(){
         holdDown = false;
         loopBar = 2;
         powerLvlFunc(increase);
-        return;
-      });
+        increase = 0;
+       console.log("2nd");
+        powerButton.removeEventListener('mousedown', powerBar);
+      
+      }, { once: true });
+      
+   
       //up
       while(loopBar === 1){
+  
         await sleep(speedOfMeter);
         powerMeter.style.height = increase + "%";
         if (increase == 100){
+          console
           loopBar--;
         }
         increase++;
 
       }//down
         while(loopBar === 0){
+
         await sleep(speedOfMeter);
         powerMeter.style.height = increase + "%";
         if (increase == 0){
@@ -54,43 +165,134 @@ async function powerBar(){
         }
         increase--;
       }
+
     }
+    console.log("TESTSETSTESTE")
+    return;
     
 
+}
+let getDistance = function(x1, y1, x2, y2){
+  var result = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+  return result;
 }
 // --------------------------------------------------------
 //STEP 2---------------------------------------------------------
 //assigns value 0-9 based off power level
 function powerLvlFunc(power){
+  let powerString = power.toString();
   if(power < 10){
      powerLevel.textContent = 0;
-     
+     phoneNum = 0;
 
   }
   else{
-    let powerString = power.toString();
+    
     powerLevel.textContent = powerString[0];
+    phoneNum = Number(powerString[0]);
+   
   
   }
-  setSpeed(power);
   
-  testButton.addEventListener('click', update);
+  // setSpeed(powerString, power);
+  lineAnimation();
+  
+ 
+  
+  
   
    
 }
 
-function setSpeed(power){
-  if (power <= 50){
-    speed = 1;
-  }
-  else if (power > 50)
-    speed = 5;
+// function setSpeed(powerString, power){
+//   console.log(powerString);
+//   if (power < 10){
+//     speed = 10;
+//     return;
+//   }
+//   if (power === 100){
+//     speed = 4;
+//     return;
+//   }
+
+
+//   switch(powerString[0]){
+//     case "1": 
+//       speed = 0.5;
+//       break;
+//     case "2": 
+//       speed = 1;
+//       break;
+//     case "3": 
+//       speed = 1.5;
+//       break;
+//     case "4": 
+//       speed = 1.5;
+//       break;
+//     case "5": 
+//       speed = 2;
+//       break;
+//     case "6": 
+//       speed = 2.5;
+//       break;
+//     case "7": 
+//       speed = 2.5;
+//       break;
+//     case "8": 
+//       speed = 3;
+//       break;
+//     case "9": 
+//       speed = 3.5;
+//       break;
+
+//   }
+//   return;
+
   
-}
+
+// }
 
 //--------------------------------------------------------------------
 
+// function update(goal) {
+ 
+      
+//   // ctx.clearRect(20, 20, canvas.width, canvas.height); // Clear canvas
+ 
+//   ball.x += ball.dx;
+//   ball.y += ball.dy;
 
+
+//         // Example: Bounce off walls (adjust as needed for your game logic)
+//     if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
+//             ball.dx *= -1;
+//     }
+//     if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+//             ball.dy *= -1;
+//     }
+
+//     drawBall(); // Redraw the ball
+//     if(ballKill === 1){
+//       ctx.clearRect(0, 0, canvas.width, canvas.height); 
+//       return;
+      
+//     }
+//     console.log("testing: " + ball)
+//     requestAnimationFrame(update); // Continue the loop
+//     }
+
+    
+// function drawBall(){
+//   ctx.beginPath();
+//   ctx.arc(ball.x, ball.y, ball.radius, 0, 2*Math.PI );
+//   ctx.fillStyle = ball.color;
+//   ctx.fill();
+//   ctx.closePath()
+ 
+
+  
+// }
+    
 
 
 
@@ -98,57 +300,29 @@ function setSpeed(power){
 //-----------------------------------------
 
 
-function drawBall(){
-  ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ball.radius, 0, 2*Math.PI );
-  ctx.fillStyle = ball.color;
-  ctx.fill();
-  ctx.closePath()
- 
-
-  
-}
 
 
-    canvas.addEventListener('click', async function(event) {
+    // canvas.addEventListener('click', async function(event) {
      
-      console.log("TEST")
-        // Calculate direction towards mouse click
-        let angle = Math.atan2(event.clientY - ball.y, event.clientX - ball.x);
-        ball.dx = Math.cos(270* Math.PI / 180) * speed;
-        ball.dy = -Math.sin(1 * Math.PI / 180) * speed;
+    //   console.log("TEST")
+    //     // Calculate direction towards mouse click
+    //     let angle = Math.atan2(event.clientY - ball.y, event.clientX - ball.x);
+    //     ball.dx = Math.cos(180 * Math.PI / 180) * speed;
+    //     ball.dy = -Math.sin(20 * Math.PI / 180) * speed;
        
         
-        await sleep(1000);
-        ball = {
-        x: canvas.width/2,
-        y: canvas.height -30,
-        radius: 5,
-        color: "green",
-        dx: 0,
-        dy: 0,};
+    //     await sleep(1000);
+    //     ball = {
+    //     x: canvas.width/2,
+    //     y: canvas.height -30,
+    //     radius: 5,
+    //     color: "green",
+    //     dx: 0,
+    //     dy: 0,};
 
 
-    });
-    function update() {
-      
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
+    // });
 
-        ball.x += ball.dx;
-        ball.y += ball.dy;
-
-        // Example: Bounce off walls (adjust as needed for your game logic)
-        if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
-            ball.dx *= -1;
-        }
-        if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
-            ball.dy *= -1;
-        }
-
-        drawBall(); // Redraw the ball
-        requestAnimationFrame(update); // Continue the loop
-    }
-    
 
     
 
@@ -159,12 +333,65 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
+let bounce = 0;
+let loopChecker = 1;
+let ballKill = 0;
+
+let goal = new Goals(0, 0, 50, "green", phoneNum);
+
 async function lineAnimation(){
-  let loopChecker = 1;
-  let bounce = 0;
-  let angle = 90;
+  goal.x = getRandomInteger();
+  goal.y = getRandomInteger();
+  undraw = 0;
+  ball.draw();
+  goal.draw();
+  line.style.visibility = "visible";
+  ballKill = 0;
+  loopChecker = 1;
+  bounce = 0;
+  angle = 90;
+   
+  
 
   while (loopChecker === 1){
+    
+    canvas.addEventListener('click', async function(event) {
+
+      console.log("TEST")
+        // Calculate direction towards mouse click
+        loop();
+        let rad = (angle - 90) * Math.PI / 180;
+        let dirX = -Math.cos(rad);
+        let dirY = -Math.sin(rad);
+
+        console.log( ball.velY);
+        ball.velX = dirX * speed;
+        ball.velY = dirY * speed;
+        console.log( ball.velX);
+        console.log( "speed: " + speed );
+        
+
+         line.style.visibility = "hidden";
+        await sleep(2000);
+        undraw = 1;
+        ball.velX = 0;
+        ball.velY = 0;
+        await sleep(100)
+        ballKill = 1;
+          bounce = 2;
+        loopChecker = 2;
+        
+        
+
+
+        
+       
+       
+
+
+    }, { once: true });
+
     while(bounce === 0){
       if (angle > 270)
       {
@@ -172,7 +399,7 @@ async function lineAnimation(){
       }
       else{
         angle++;
-        await sleep(speedOfMeter);
+        await sleep(3);
         line.style.transform = "rotate(" + angle +"deg)";
       }
     while(bounce === 1){
@@ -183,13 +410,22 @@ async function lineAnimation(){
       }
       else{
         angle--
-        await sleep(speedOfMeter);
+        await sleep(3);
+        
         line.style.transform = "rotate(" + angle +"deg)";
       }
     }
     }
 
   }
+  
+  powerButton.addEventListener("mousedown", powerBar);
+  console.log("HOLD IT")
+
+ 
+ 
+ 
+  return;
   
 }
 
